9467f5b0dfaa49d6ca19b39eb471373d33848d6b
rafdty <rafadty694@gmail.com>
2023-06-07 01:56:21 +0700

    commit tugas 12 introduce laravel

---
+++ Tugas 12 Laravel/resources/views/lamanku/Kontak.blade.php
@@ -0,0 +1,28 @@
+<p>
+<h1>BUTUH BANTUAN? </h1>
+</p>
+
+<p>Ini Adalah Halaman Kontak</p>
+
+<style>
+	body {
+		background-color: lightblue;
+	}
+</style>
+
+<table border="1">
+	<tr>
+		<td>Email</td>
+		<td>:</td>
+		<td>akmalfuad7@gmail.com</td>
+	</tr>
+	<tr>
+		<td>Hp</td>
+		<td>:</td>
+		<td>0881-xxxx-xxxx</td>
+	</tr>
+</table>
+
+<footer>
+	<p>&copy; <a href=> @akmalfuadhasan.com</a>. 2023 - 2024</p>
+</footer>
